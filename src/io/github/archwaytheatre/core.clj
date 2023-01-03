7d0f6cd9055276914e877f91f3188eaf51370fa5
rachel <rkwestmacott@gmail.com>
2023-01-03 18:19:01 +0000

    Favicon

--- src/io/github/archwaytheatre/core.clj
+++ src/io/github/archwaytheatre/core.clj
@@ -52,6 +52,7 @@
          [:meta {:charset "utf-8"}]
          [:title title]
          [:link {:rel "stylesheet" :href "./css/style.css"}]
+         [:link {:rel "icon" :type "image/png" :href "favicon.png"}]
          [:script {:src "./js/popup.js"}]]
         [:body
          [:div.prenav
