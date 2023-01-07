2330c5a25850d6d6705814319577eabbc6859161
rachel <rkwestmacott@gmail.com>
2023-01-07 23:43:48 +0000

    Move contact page content into a table.

--- src/io/github/archwaytheatre/site/contact.clj
+++ src/io/github/archwaytheatre/site/contact.clj
@@ -1,30 +1,36 @@
 (ns io.github.archwaytheatre.site.contact
-  (:require [io.github.archwaytheatre.site.core :as core]))
+  (:require
+    [clojure.string :as string]
+    [io.github.archwaytheatre.site.core :as core]))
 
+(defn table [rows]
+  (into [:div.table {:role "grid"}]
+        (for [row rows]
+          (let [[title description] row]
+            [:div.row {:role "row"}
+             [:div.cell {:role  "gridcell"
+                         :style "font-size:larger;"}
+              (string/replace title " " "&nbsp;")]
+             [:div.cell.noglow {:role "gridcell"} description]]))))
 
 (core/page "contact" "The Archway Theatre"
   [:div
 
    [:div.container
-    [:div "You can get in touch with us by post, email, phone or using the contact form below"]
+    [:div "You can get in touch with us by post, email, phone or using the contact form below."]
     [:br]
-    [:div [:b "Box Office Online:"] [:a {:href "https://www.ticketsource.co.uk/archwaytheatre/"} "ticketsource.co.uk"]]
-    [:div [:b "Box Office Phone:"]
-     [:a {:href "tel:0333-666-3366"} "0333 666 3366"]
-     "(handled by TicketSource)"]
-    [:div {:style "margin-left: 40px"}
-     [:div "Open: Mon-Fri 9:00-19:00, Sat 9:00-17:00"]
-     [:div "£1.75 phone booking fee. Call charges may also apply."]]
-    [:br]
-    [:div [:b "General enquiries:"]
-     [:a {:href "tel:01293-784-398"} "(01293) 784 398"]]
-    [:br]
-    [:div [:b "Address:"]]
-    [:div "Archway Theatre Company Limited"]
-    [:div "The Drive"]
-    [:div "Horley"]
-    [:div "Surrey"]
-    [:div "RH6 7NQ"]]
+    (table [["Box Office Online" [:a {:href "https://www.ticketsource.co.uk/archwaytheatre/"} "ticketsource.co.uk"]]
+            ["Box Office Phone" [:div
+                                 [:div [:a {:href "tel:0333-666-3366"} "0333 666 3366"] "(handled by TicketSource)"]
+                                 [:div "Open: Mon-Fri 9:00-19:00, Sat 9:00-17:00"]
+                                 [:div "£1.75 phone booking fee. Call charges may also apply."]]]
+            ["General enquiries" [:a {:href "tel:01293-784-398"} "(01293) 784 398"]]
+            ["Address" [:pre (string/join "\n"
+                               ["Archway Theatre Company Limited"
+                                "The Drive"
+                                "Horley"
+                                "Surrey"
+                                "RH6 7NQ"])]]])]
 
    [:iframe#JotFormIFrame-230046557100040
     {:title "Contact Us"
@@ -37,7 +43,7 @@
       min-width: 100%;
       height:100vh;
       border:none;"
-     ;:scrolling "no"
+     :scrolling "no"
      }]
    [:script {:type "text/javascript"} "var ifr = document.getElementById(\"JotFormIFrame-230046557100040\");
     if (ifr) {
