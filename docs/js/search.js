ba83076ec2b29f20688fc1bb22db3e65fbb90b62
rachel <rkwestmacott@gmail.com>
2023-07-08 08:18:58 +0100

    Fix search bug.

--- docs/js/search.js
+++ docs/js/search.js
@@ -5,7 +5,7 @@ var thisSite = 'archwaytheatre.github.io'
 function updateLink(inputElement, elementId) {
     console.log(inputElement.value);
     document.getElementById(elementId).href =
-        'https://duckduckgo.com/?q=site%3' + thisSite + '+' +
+        'https://duckduckgo.com/?q=site%3A' + thisSite + '+' +
         encodeURIComponent(inputElement.value).replaceAll('%20', '+');
     console.log(document.getElementById(elementId).href);
 }
