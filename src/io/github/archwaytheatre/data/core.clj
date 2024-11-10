e90f4e14e10e09bb3b8b79202aff73b46c398a24
rachel <rkwestmacott@gmail.com>
2024-11-10 21:39:03 +0000

    Dry definitions.

--- src/io/github/archwaytheatre/data/core.clj
+++ src/io/github/archwaytheatre/data/core.clj
@@ -6,7 +6,10 @@
 
 (def data-dir (io/file "data"))
 (def event-dir (io/file data-dir "events"))
-(def s3-dir "s3://archwaytheatre/site")
+(def s3-protocol "s3://")
+(def s3-bucket "archwaytheatre")
+(def s3-dir (str s3-protocol s3-bucket "/site"))
+(def s3-temp-dir (str s3-protocol s3-bucket "/temp"))
 (def _asset-url-prefix "https://archwaytheatre.s3.eu-west-2.amazonaws.com/site/")
 (def asset-url-prefix "https://d35974u23x7pxe.cloudfront.net/")
 
