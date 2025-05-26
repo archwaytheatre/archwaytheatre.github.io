b4b3067f3292fa3d80f1fdac42c5a602319b90bb
rachel <rkwestmacott@gmail.com>
2025-05-26 11:40:39 +0100

    Functioning link on the 'programmes' page.

--- src/io/github/archwaytheatre/site/programmes.clj
+++ src/io/github/archwaytheatre/site/programmes.clj
@@ -6,8 +6,10 @@
   [:div.content
 
    [:div.content__item
-    [:h1 "Programmes"]]
+    [:h1 "Digital Programmes"]
+    [:div "Click / Tap to download."]]
 
    [:div.content__item
     [:ul
-     [:li "Production programmes will appear here..."]]]])
+     [:li [:a {:href "https://d35974u23x7pxe.cloudfront.net/2025/theit/The+IT+final.pdf"}
+     "The IT Digital Programme (PDF Download)"]]]]])
