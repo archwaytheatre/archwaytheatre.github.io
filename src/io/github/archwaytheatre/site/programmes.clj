feb6086296a244523ed1f297f302bb3bd2577f0a
rachel <rkwestmacott@gmail.com>
2025-09-09 08:50:04 +0100

    Heartburn programme.

--- src/io/github/archwaytheatre/site/programmes.clj
+++ src/io/github/archwaytheatre/site/programmes.clj
@@ -7,9 +7,24 @@
 
    [:div.content__item
     [:h1 "Digital Programmes"]
-    [:div "Click / Tap to download."]]
+    [:div.center "Click / Tap to download the digital PDF."]]
 
    [:div.content__item
+    ; Just the one latest / current production goes here:
+    [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/heartburn/HeartburnProgramme.pdf"}
+     "Heartburn Digital Programme"]
+
+    ]
+
+   [:div.content__item
+    [:h5 "Previous Productions"]
     [:ul
-     [:li [:a {:href "https://d35974u23x7pxe.cloudfront.net/2025/theit/The+IT+final.pdf"}
-     "The IT Digital Programme (PDF Download)"]]]]])
+
+     ;; All past productions go here:
+     #_[:li [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/heartburn/HeartburnProgramme.pdf"}
+     "Heartburn Digital Programme"]]
+
+     [:li [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/theit/The+IT+final.pdf"}
+     "The IT Digital Programme"]]
+
+     ]]])
