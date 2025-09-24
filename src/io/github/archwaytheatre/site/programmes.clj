ab6174abfff4890b975e0d83b47a8f02d8e268a6
rachel <rkwestmacott@gmail.com>
2025-09-24 14:39:39 +0100

    Programmes with image.

--- src/io/github/archwaytheatre/site/programmes.clj
+++ src/io/github/archwaytheatre/site/programmes.clj
@@ -1,5 +1,20 @@
 (ns io.github.archwaytheatre.site.programmes
-  (:require [io.github.archwaytheatre.site.core :as core]))
+  (:require ;[io.github.archwaytheatre.data.plays :as plays]
+            [io.github.archwaytheatre.site.core :as core]))
+
+
+;(defonce plays (plays/get-productions-for ...))
+; todo: plays/get-current-and-past-productions? - then filter current? or maybe separate fns for current and past!
+; todo: lose the words 'digital programme' from every link?
+; todo: extract PDF cover image from PDFs...?
+
+(defn programme-link [plays production-name]
+  ; todo: should derive path and text from production
+  [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/nellgwynn/NellGwynnProgramme.pdf"}
+   [:div
+    [:div.text-align-center "Nell Gwynn Digital Programme"]
+    [:br]
+    [:img.programme-link__poster {:src "https://d35974u23x7pxe.cloudfront.net/2025/nellgwynn/poster-scaled.png"}]]])
 
 
 (core/page-2 "programmes" "Digital Programmes"
@@ -14,8 +29,7 @@
      ; Just the one latest / current production goes here:
 
     [:div.current-programme
-     [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/nellgwynn/NellGwynnProgramme.pdf"}
-      "Nell Gwynn Digital Programme"]]
+     (programme-link "" "")]
 
     ]
 
@@ -24,7 +38,7 @@
     [:ul
 
      ;; All past productions go here...
-
+     ; todo: should have separate helper function to derive path from production name and create previous links
      [:li [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/heartburn/HeartburnProgramme.pdf"}
      "Heartburn Digital Programme"]]
 
