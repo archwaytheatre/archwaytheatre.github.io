e040514bbc14e1c527e31155f0270763fcbd5fd9
rachel <rkwestmacott@gmail.com>
2025-09-24 13:21:26 +0100

    Programme for Nell Gwynn

--- src/io/github/archwaytheatre/site/programmes.clj
+++ src/io/github/archwaytheatre/site/programmes.clj
@@ -6,14 +6,16 @@
   [:div.content
 
    [:div.content__item
-    [:h1 "Digital Programmes"]
-    [:div.center "Click / Tap to download the digital PDF."]]
+    [:h1.text-align-center "Digital Programmes"]
+    [:p "Click / Tap to download the digital PDF."]
+    [:p "Note that programmes are free to download, but that there may be a charge when purchasing physical copies."]]
 
    [:div.content__item
      ; Just the one latest / current production goes here:
+
     [:div.current-programme
-     [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/heartburn/HeartburnProgramme.pdf"}
-      "Heartburn Digital Programme"]]
+     [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/nellgwynn/NellGwynnProgramme.pdf"}
+      "Nell Gwynn Digital Programme"]]
 
     ]
 
@@ -21,8 +23,9 @@
     [:h5 "Previous Productions"]
     [:ul
 
-     ;; All past productions go here:
-     #_[:li [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/heartburn/HeartburnProgramme.pdf"}
+     ;; All past productions go here...
+
+     [:li [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/heartburn/HeartburnProgramme.pdf"}
      "Heartburn Digital Programme"]]
 
      [:li [:a.normal {:href "https://d35974u23x7pxe.cloudfront.net/2025/theit/The+IT+final.pdf"}
