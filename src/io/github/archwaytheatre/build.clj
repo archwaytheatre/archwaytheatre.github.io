5551a4f224582eebc520408495c14a102538703e
rachel <rkwestmacott@gmail.com>
2026-01-31 18:37:51 +0000

    Always rebuild the audition page.

--- src/io/github/archwaytheatre/build.clj
+++ src/io/github/archwaytheatre/build.clj
@@ -3,19 +3,23 @@
             [clojure.string :as string]))
 
 
+(def always-rebuild
+  (map io/file
+       ["./src/io/github/archwaytheatre/site/index.clj"
+        "./src/io/github/archwaytheatre/site/whatson.clj"
+        "./src/io/github/archwaytheatre/site/auditions.clj"]))
+
 (defn discover-namespaces [files]
   (sequence
     (comp (remove #(-> % .getPath (string/includes? "content")))
           (filter #(-> % .getName (string/ends-with? ".clj")))
-          (map (fn [file]
-                 (->> file slurp string/split-lines first
-                      (re-find #"io.github.archwaytheatre.site.[\S]+")
-                      symbol))))
-    (cons
-      (io/file "./src/io/github/archwaytheatre/site/index.clj")
-      (cons (io/file "./src/io/github/archwaytheatre/site/whatson.clj")
+          (keep (fn [file]
+                  (some->> file slurp string/split-lines first
+                           (re-find #"io.github.archwaytheatre.site.[\S]+")
+                           symbol))))
+    (concat always-rebuild
             (or (seq (remove nil? files))
-                (file-seq (io/file "./src/io/github/archwaytheatre/site")))))))
+                (file-seq (io/file "./src/io/github/archwaytheatre/site"))))))
 
 (defn require-namespaces [files]
   (let [nss (vec (discover-namespaces files))]
